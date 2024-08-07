4777f05f9407cabc8c416316096df68e6a2d3635
Mark Bautista <42196376+tazki@users.noreply.github.com>
2024-08-07 15:00:39 +0800

    [Add] Implement Email Verification

---
+++ app/(auth)/new-verification/page.tsx
@@ -0,0 +1,7 @@
+import { NewVerificationForm } from "@/app/_components/auth/new-verification-form";
+
+const NewVerificationPage = () => {
+  return <NewVerificationForm />;
+};
+
+export default NewVerificationPage;
--- lib/token.ts
+++ lib/token.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 import { db } from "@/lib/db";
-import { getVerificationTokenByEmail } from "@/data/verifcation-token";
+import { getVerificationTokenByEmail } from "@/data/verification-token";
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
