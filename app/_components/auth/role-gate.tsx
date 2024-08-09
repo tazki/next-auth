0bc93039a4cbcfaa949d3b36def02c45c1fb5ed5
Mark Bautista <42196376+tazki@users.noreply.github.com>
2024-08-09 08:32:32 +0800

    [Add] Implement Role Gates

---
+++ app/_components/auth/role-gate.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { UserRole } from "@prisma/client";
+import { useCurrentRole } from "@/hooks/user-current-role";
+import { FormError } from "@/app/_components/form-error";
+
+interface RoleGateProps {
+  children: React.ReactNode;
+  allowedRole: UserRole;
+}
+
+export const RoleGate = ({ children, allowedRole }: RoleGateProps) => {
+  const role = useCurrentRole();
+
+  if (role !== allowedRole) {
+    return (
+      <FormError message="You do not have permission to view this content!" />
+    );
+  }
+
+  return <>{children}</>;
+};
